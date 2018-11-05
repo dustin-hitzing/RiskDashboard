077d6671d29801962b0db53fe2cf958f202ba7df
DESKTOP-OVLQ66P\New Guy <dustinhitzing@gmail.com>
2018-11-04 19:15:37 -0500

    push to Abraham

---
+++ CompanyWeatherAssessmentVR/Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ApplicationManager : MonoBehaviour
+{
+    public TextMeshProUGUI StateText;
+    public TextMeshProUGUI CityText;
+    public TextMeshProUGUI AddressText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StateText.text = "State: TX";
+        CityText.text = "City: Houston";
+        AddressText.text = "Address: 123 Awesometon Rd";
+    }
+}
---
+++ CompanyWeatherAssessmentVR/Assets/Scripts/CanvasLookAtCamera.cs
@@ -0,0 +1,15 @@
+﻿using AT.XR.RiskManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasLookAtCamera : MonoBehaviour
+{
+    public Camera XRCamera;
+    void Update()
+    {
+        transform.LookAt(XRCamera.transform);
+        //var xrRotation = XRCamera.transform.rotation;
+        //XRCamera.transform.Rotate(new Vector3(xrRotation.x, xrRotation.y, 180f));
+    }
+}
